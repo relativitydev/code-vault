efee311893201e74d4e625aef9997fbfc5ca1206
Albert Yu <36044494+a-yu-relativity@users.noreply.github.com>
2018-05-31 10:48:22 -0500

    Create load-cdn.js

---
+++ JavaScript/amd/load-cdn.js
@@ -0,0 +1,33 @@
+// This example shows how to load Bootstrap from a CDN
+// using RequireJS in an AMD.
+
+// First, specify the Bootstrap CDN in the RequireJS config
+requirejs.config({
+    paths: {
+        "bootstrap": ["//stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min"]
+    },
+    shim: {
+        "bootstrap": {
+            deps: ["jquery"],
+            exports: "$"
+        }
+    }
+});
+
+// Then, specify both jQuery and Bootstrap as dependencies
+define(["jquery", "bootstrap"], function ($) {
+    var config = requirejs.s.contexts._.config;
+    console.log(config);
+
+    if (typeof($.fn) !== 'undefined') {
+        console.log("Loaded bootstrap");
+        var bootstrap_ver = $.fn.tooltip.Constructor.VERSION;
+        console.log(bootstrap_ver);  // => 4.1.1
+    }
+
+    function sampleHandler(api) {
+        // blah blah
+    }
+    return sampleHandler;
+});
+
