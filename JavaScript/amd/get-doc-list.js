dcdb24d002e7594b90484466f86496410f6400e3
Albert Yu <36044494+a-yu-relativity@users.noreply.github.com>
2018-09-20 16:06:32 -0500

    Change spacing to 2

--- JavaScript/amd/get-doc-list.js
+++ JavaScript/amd/get-doc-list.js
@@ -1,40 +1,40 @@
-define(function () {
-   function sampleHandler(api) {
-       /*
-        * Gets the list of the currently filtered docs,
-        * be it from a view or saved search
-        */ 
-       function getFilteredDocs(overrideApi) {
-           // set a custom data source factory function for item list
-           overrideApi.setItemListDataSource(function (itemListDataSourceParams) {               
-               return {
+define(function() {
+  function sampleHandler(api) {
+    /*
+     * Gets the list of the currently filtered docs,
+     * be it from a view or saved search
+     */
+    function getFilteredDocs(overrideApi) {
+      // set a custom data source factory function for item list
+      overrideApi.setItemListDataSource(function(itemListDataSourceParams) {
+        return {
 
-                   inboundTransformer: {
-                       method: function (data) {
-                           console.log(data);
-                           // to get the list of artifact ids:
-                           var artifactIds = data["IDWindow"];
-                           return api.inboundTranslationService.objectManager.translate(
-                               itemListDataSourceParams.workspaceId,
-                               itemListDataSourceParams.folderId,
-                               data,
-                               itemListDataSourceParams.callbacks.dataMappingCallback,
-                               itemListDataSourceParams.callbacks.docReviewPrimingCallback,
-                               itemListDataSourceParams.callbacks.refreshFILCallback,
-                               itemListDataSourceParams.callbacks.getGroupDefinitionFieldName);
-                       }
-                   }
-                    
-               };
-           });
-       }
+          inboundTransformer: {
+            method: function(data) {
+              console.log(data);
+              // to get the list of artifact ids:
+              var artifactIds = data["IDWindow"];
+              return api.inboundTranslationService.objectManager.translate(
+                itemListDataSourceParams.workspaceId,
+                itemListDataSourceParams.folderId,
+                data,
+                itemListDataSourceParams.callbacks.dataMappingCallback,
+                itemListDataSourceParams.callbacks.docReviewPrimingCallback,
+                itemListDataSourceParams.callbacks.refreshFILCallback,
+                itemListDataSourceParams.callbacks.getGroupDefinitionFieldName);
+            }
+          }
 
-       // "subscribe" to the events--that is, handle them
-       return {
-           // handle the dataSourceInit event, which occurs
-           // once upon page load
-           dataSourceInit: getFilteredDocs
-       };
-   }
-   return sampleHandler;
+        };
+      });
+    }
+
+    // "subscribe" to the events--that is, handle them
+    return {
+      // handle the dataSourceInit event, which occurs
+      // once upon page load
+      dataSourceInit: getFilteredDocs
+    };
+  }
+  return sampleHandler;
 });
